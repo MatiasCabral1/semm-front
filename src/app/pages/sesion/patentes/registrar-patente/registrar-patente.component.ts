540cd3078eea6e721b8da73b264b5dbf5082a962
Matias <matiasagustincabral976@gmail.com>
2022-01-03 10:52:26 -0300

    implementacion front expresiones regulares

--- src/app/pages/sesion/patentes/registrar-patente/registrar-patente.component.ts
+++ src/app/pages/sesion/patentes/registrar-patente/registrar-patente.component.ts
@@ -4,6 +4,7 @@ import { modelPatente } from 'src/app/models/modelPatente';
 import { nuevaPatente } from 'src/app/models/nueva-patente';
 import { PatenteService } from 'src/app/service/patente.service';
 import { TokenService } from 'src/app/service/token.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registrar-patente',
@@ -14,8 +15,7 @@ export class RegistrarPatenteComponent implements OnInit {
  numero!: any;
  usuarioPatente! : nuevaPatente;
  expresiones = {
-   tipo1: /\D{3}\d{3}/,
-   tipo2: /\D\D\d\d\d\D\D/gm
+   tipo1: /([a-zA-Z]{3}\d{3})|([a-zA-Z]{2}\d{3}[a-zA-Z]{2})/,
  }
 
   constructor(
@@ -28,15 +28,36 @@ export class RegistrarPatenteComponent implements OnInit {
   }
 
   registrarPatente(){
-    this.usuarioPatente = new nuevaPatente(this.numero,this.tokenService.getUserName()!);
-    this.patenteService.create(this.numero,this.tokenService.getUserName()!).subscribe();
-    this.router.navigateByUrl("/listadoPatentes");
+    if(this.validarExpresiones(this.numero)){
+      console.log("cumple");
+      this.usuarioPatente = new nuevaPatente(this.numero,this.tokenService.getUserName()!);
+      this.patenteService.create(this.numero,this.tokenService.getUserName()!).subscribe();
+      this.router.navigateByUrl("/listadoPatentes");
+    }
   }
 
-  validarExpresiones(){
-    let patente= "aaa29922"; 
-    const validation = patente.match(this.expresiones.tipo1);
-      console.log("cumple con la expresion",validation?.values().next().value);
+  validarExpresiones(patente: any): boolean{
+    //valida que la patente ingresada cumpla con el formato tipo1
+    // si no lo cumple lo informa en pantalla.
+    //en validation[0] se almacena el valor que hizo coincidencia con la expresion. 
+    //si el valor almacenado es distinto de la patente entonces la patente ingresada contiene mas caracteres que los pedidos en la expresion. Por lo tanto es incorrecta
+    const validation1 = patente.match(this.expresiones.tipo1);
+    if(validation1 != null){
+      if(validation1[0] == patente){
+        return true;
+      }else{
+        this.errorNotificationExpresion();
+        return false;
+      }
+    }
+    //emitir alerta de que formato debe cumplir.
+    this.errorNotificationExpresion();
+    return false;
+      
+  }
+  
+  errorNotificationExpresion(){
+    Swal.fire('El formato ingresado es invalido', 'Ejemplos de formatos aceptados: "AAA999" - "AA000AA"', 'error')
   }
   //expresiones regulares AAA999 -> /\D\D\D\d\d\d/gm
   //formato mercosur -> /\D\D\d\d\d\D\D/gm
